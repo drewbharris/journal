00c94ec7c49b62e838bc5c0b96abccf8624c2291
Drew Harris <contact@grmnygrmny.com>
2011-08-05 21:56:20 -0500

    started building webpage

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Post(models.Model):
+	username = models.CharField(max_length=20)
+	anonymous = models.BooleanField()
+	private = models.BooleanField()
+	image = models.BooleanField()
+	body = models.TextField()
+	created = models.DateTimeField()
+
+	def __unicode__(self):
+		return self.username
+
+class Test(models.Model):
+	test = 'test message'
+	def __unicode__(self):
+		return self.test
+
+# Admin
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('anonymous', 'private', 'image', 'username', 'created')
+	fields = ["username", "anonymous", "private", "image", "created", "body"]
+
+admin.site.register(Post, PostAdmin)
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from main.models import Test
+
+def index(request):
+    var = Test.test
+    t = loader.get_template('main/index.html')
+    c = Context({ 'tempvars': var, 'tempvar2': 'helloworld2'})
+    return HttpResponse(t.render(c))
+
+def index(request):
+    t = loader.get_template('main/post.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
